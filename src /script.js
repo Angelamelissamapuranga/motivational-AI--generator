beeec5740784cc18afa7c625f077bd2f87759377
Angelamelissamapuranga <angelamapuranga68@gmail.com>
2025-10-07 00:19:46 +0200

    updated user experience

--- src /script.js
+++ src /script.js
@@ -17,6 +17,11 @@ function motivation(event) {
     "You are a motivational expert, you push people by giving them uplifting qoutes, your mission is to generate qoutes less than 4 lines, seperate each line with <br />, do not show the html words on the screen only show the format, sign off at the end of the poem 'Angela Mapuranga'in <strong />";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let motivationElement = document.querySelector("#poem");
+  motivationElement.classList.remove("hidden");
+  motivationElement.innerHTML = `⏳generating motivational qoute about ${instructionsInput.value}`;
+
   axios.get(apiUrl).then(displayMotivation);
 }
 
